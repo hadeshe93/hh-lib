9832b5b0c746734d2e44db6d77f9e45a0ef536ee
hadeshe93 <hadeshe93@gmail.com>
2021-06-12 09:49:27 +0800

    feat: node 包新增下载模块

---
+++ packages/node/src/download/index.ts
@@ -0,0 +1,41 @@
+import { typeModule } from '@hadeshe93/lib-common';
+import fsExtra from 'fs-extra';
+import axios from 'axios';
+
+import fs from 'fs';
+import path from 'path';
+
+/**
+ * 下载资源函数
+ * @param url 资源地址
+ * @param dest 本地目标地址
+ * @param extName 选填，可以重新指定后缀名
+ */
+export const downloadAsset = async (url: string, dest: string, extName?: string): Promise<void> => {
+  const { dir: destDir } = path.parse(dest);
+  const { name, ext: oriExt } = path.parse(url);
+  let ext = oriExt;
+  if (typeModule.getTypeOf(extName) === 'string') {
+    ext = extName;
+  }
+  const destFilePath = path.join(destDir, `${name}.${ext}`);
+  await fsExtra.ensureDir(destDir);
+  await downloadCore(url, destFilePath);
+};
+
+/**
+ * 核心下载逻辑
+ * @param src 资源源地址
+ * @param dest 本地目标地址
+ */
+async function downloadCore(src: string, dest: string) {
+  if (!/^http(s)?:\/\//.test(src)) {
+    throw new Error(`Error occurred when download asset: ${src} is not a effective link`);
+  }
+
+  // 线上地址走下载
+  const resp = await axios.get(src, {
+    responseType: 'stream',
+  });
+  resp.data.pipe(fs.createWriteStream(dest));
+}
--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,1 +1,2 @@
 export * as ip from './ip/index';
+export * as download from './download/index';
