84b32e1388174ca370031b7cac0fbc3320df1be8
hadeshe93 <hadeshe93@gmail.com>
2022-06-17 16:28:07 +0800

    test: 增加单测

--- packages/browser/__tests__/src/browser/index.test.ts
+++ packages/browser/__tests__/src/browser/index.test.ts
@@ -1,4 +1,4 @@
-import { checkIsMobile } from '@/browser/index';
+import { checkIsMobile, getMobileOSTypeMap } from '@/browser/index';
 
 describe('针对 checkIsMobile 方法的测试集', () => {
   test('checkIsMobile 能检测移动端', () => {
@@ -9,5 +9,11 @@ describe('针对 checkIsMobile 方法的测试集', () => {
     const DESKTOP_CHROME_UA =
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36';
     expect(checkIsMobile(DESKTOP_CHROME_UA)).toBe(false);
+  });
+});
+
+describe('针对 getMobileOSTypeMap 方法的测试集', () => {
+  test('getMobileOSTypeMap 能正确检测移动端', () => {
+    expect(getMobileOSTypeMap().ios).toBe(true);
   });
 });
