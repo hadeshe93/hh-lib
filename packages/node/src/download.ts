42954cc9917091a0b41b17abe3bbe97c3b1aa921
hadeshe93 <hadeshe93@gmail.com>
2022-07-01 16:53:45 +0800

    feat: 添加 node 包相关新特性

--- packages/node/src/download.ts
+++ packages/node/src/download.ts
@@ -1,8 +1,54 @@
 import fs from 'fs';
 import path from 'path';
+import ora from 'ora';
 import axios from 'axios';
 import fsExtra from 'fs-extra';
+import download, { DownloadOpts } from 'download-git-repo';
 import { getTypeOf } from '@hadeshe93/lib-common';
+
+interface DownloadGitRepoOpts extends DownloadOpts {
+  ora?: boolean;
+}
+
+/**
+ * 下载 git 仓库到本地
+ *
+ * @export
+ * @param {string} repoFullName
+ * @param {string} dest
+ * @param {DownloadGitRepoOpts} [rawOpts]
+ * @return {*}  {(Promise<undefined | Error>)}
+ */
+export async function downloadGitRepo(
+  repoFullName: string,
+  dest: string,
+  rawOpts?: DownloadGitRepoOpts,
+): Promise<undefined | Error> {
+  rawOpts = rawOpts
+    ? {
+        ora: false,
+        clone: false,
+        ...(rawOpts || {}),
+      }
+    : rawOpts;
+  const { ora: needOra, ...opts } = rawOpts || {};
+
+  let sipnner;
+  if (needOra) {
+    sipnner = ora('Downloading ...').start();
+  }
+  return new Promise((resolve, reject) => {
+    download(repoFullName, dest, opts, function (err) {
+      if (err) {
+        sipnner && sipnner.fail('Dowload failed.');
+        reject(err);
+        return;
+      }
+      sipnner && sipnner.succeed('Dowload successfully!');
+      resolve(void 0);
+    });
+  });
+}
 
 /**
  * 下载资源函数
