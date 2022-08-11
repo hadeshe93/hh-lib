85c87280cca4cfa9c39f068b222d812aff698dd1
hadeshe93 <hadeshe93@gmail.com>
2022-08-11 10:22:33 +0800

    chore: 完善基础基建

--- tasks/vice-flow/debug-command.sh
+++ tasks/vice-flow/debug-command.sh
@@ -5,10 +5,10 @@ dev_project_path="/cbs/xcode/webpack5-starter/packages/webpack5-starter-vue3-ts"
 
 if [[ $1 == "dev" ]]
 then
-  pnpm --filter @hadeshe93/vice-flow dev:flow vs:dev -- --cwd "$dev_project_path"
+  pnpm --filter @hadeshe93/vice-flow dev:flow spa:dev -- --cwd "$dev_project_path"
 elif [[ $1 == "build" ]]
 then
-  pnpm --filter @hadeshe93/vice-flow dev:flow vs:build -- --cwd "$dev_project_path"
+  pnpm --filter @hadeshe93/vice-flow dev:flow spa:build -- --cwd "$dev_project_path"
 else
   echo "没有符合的条件"
 fi
