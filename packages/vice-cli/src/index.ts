1b0fdb18950f4b49bc826d4df9f03fec0118bacd
hadeshe93 <hadeshe93@gmail.com>
2022-08-23 19:50:54 +0800

    chore: 废弃掉 vice-cli 项目

--- packages/vice-cli/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './types/vice-configs';
-export { run } from './cli/index';
-
-// 这块还要再斟酌下，到底是放到 vice-cli 里面还是 vice 里面
-// 核心问题是 vice 和 cli 到底是否需要拆开？各有什么好处？业界是怎么做的？
-// export * from 'vue';
